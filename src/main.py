3f45bcbc837d92eeae03604cd91d0a7cf3d3de2c
Yatsenko Serhii <yatsenko.serj@gmail.com>
2021-08-26 03:03:10 +0300

    Change main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
-import asyncio
 import logging
-from common import bot, dp, engine
-from models.models import Base
-from services.sheduler import scheduler
 
 
 async def main():
-    logging.basicConfig(level=logging.INFO)
-    import handlers
+    # Configure logging
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger('apsscheduler').setLevel(logging.INFO)
 
+    # Database create tables
+    from common import engine
+    from models.models import Base
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
+    # Apscheduler
+    from services.sheduler import scheduler
     scheduler.start()
 
+    # Start bot
+    import handlers
+    from common import dp, bot
     try:
         await dp.start_polling()
     finally:
@@ -24,6 +29,7 @@ async def main():
 
 
 if __name__ == '__main__':
+    import asyncio
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
